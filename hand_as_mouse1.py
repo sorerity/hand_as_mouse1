1bb576049603d68dc6723d8e6169c9ebb9e929d8
JD SOON <jdangelogsoon@gmail.com>
2024-10-02 10:14:53 +0800

    Hand Landmarks are Working

--- hand_as_mouse1.py
+++ hand_as_mouse1.py
@@ -10,10 +10,12 @@ while True:
     rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     output_hands = capture_hands.process(rgb_image)
     all_hands = output_hands.multi_hand_landmarks
-    
+
     if all_hands:
         for hand in all_hands:
             drawing_option.draw_landmarks(image,hand)
+            one_hand_landmarks = hand.landmark
+            for id, lm in enumerate(one_hand_landmarks):
     cv2.imshow("Hand Movement Video Capture",image)
     key = cv2.waitKey(100)
     if key == 27:
