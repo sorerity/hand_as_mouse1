58f5223e559599b9b93e6933f1c30a8ec7fef574
JD SOON <jdangelogsoon@gmail.com>
2024-10-02 10:23:29 +0800

    Added a Circle for the Index finger

--- hand_as_mouse1.py
+++ hand_as_mouse1.py
@@ -6,6 +6,7 @@ drawing_option = mediapipe.solutions.drawing_utils
 camera = cv2.VideoCapture(0)
 while True:
     _,image = camera.read()
+    image_height, image_width, _= image.shape
     image = cv2.flip(image,1)
     rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     output_hands = capture_hands.process(rgb_image)
@@ -16,6 +17,11 @@ while True:
             drawing_option.draw_landmarks(image,hand)
             one_hand_landmarks = hand.landmark
             for id, lm in enumerate(one_hand_landmarks):
+                current_pixel1 = int(lm.x * image_width)
+                current_pixel2 = int(lm.y * image_height)
+                if id == 8:
+                    cv2.circle(image,(current_pixel1,current_pixel2),10,(0,255,255))
+
     cv2.imshow("Hand Movement Video Capture",image)
     key = cv2.waitKey(100)
     if key == 27:
