bf6e500f5d3cffec097c4162a0dd016e0901845c
JD SOON <jdangelogsoon@gmail.com>
2024-10-02 10:09:56 +0800

    Hand Recognition is Working

--- hand_as_mouse1.py
+++ hand_as_mouse1.py
@@ -1,11 +1,23 @@
 import cv2
 import mediapipe
 import pyautogui
-
+capture_hands = mediapipe.solutions.hands.Hands()
+drawing_option = mediapipe.solutions.drawing_utils
 camera = cv2.VideoCapture(0)
 while True:
-    ret,image = camera.read()
+    _,image = camera.read()
+    image = cv2.flip(image,1)
+    rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    output_hands = capture_hands.process(rgb_image)
+    all_hands = output_hands.multi_hand_landmarks
+    
+    if all_hands:
+        for hand in all_hands:
+            drawing_option.draw_landmarks(image,hand)
     cv2.imshow("Hand Movement Video Capture",image)
     key = cv2.waitKey(100)
     if key == 27:
         break
+
+camera.release()
+cv2.destroyAllWindows()
